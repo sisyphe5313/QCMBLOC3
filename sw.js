f56f870b4c68885de31fb097fd592747a65f6754
sisyphe5313 <sisyphe5313@gmail.com>
2026-08-28 12:19:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "qcm-bloc3-site-v1";
+const CACHE = "qcm-bloc3-site-v2";
 const ASSETS = ["./","./index.html","./qcm-bloc3-data.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(k => Promise.all(k.filter(x => x !== CACHE).map(x => caches.delete(x)))).then(() => self.clients.claim())); });
